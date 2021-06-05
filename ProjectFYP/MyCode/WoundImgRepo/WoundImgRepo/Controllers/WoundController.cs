7943b4b0d2badd29c3952c3514c195187fcb20a1
CHEW TZE YANG RP <19037802@myrp.edu.sg>
2021-06-05 14:12:17 +0800

    add upload features

--- ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/WoundController.cs
+++ ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/WoundController.cs
@@ -5,45 +5,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Dynamic;
 
 namespace WoundImgRepo.Controllers
 {
     public class WoundController : Controller
     {
-        #region Index()
         public IActionResult Index()
         {
-            List<Trip> list = DBUtl.GetList<Trip>("SELECT * FROM TravelHighlight");
+            List<Wound> list = DBUtl.GetList<Wound>("SELECT * FROM wound");
             return View("Index", list);
         }
+
+        #region Details()
+        public IActionResult Details()
+        {
+            string selectSql = @"SELECT * FROM image";
+            List<Image> listImage = DBUtl.GetList<Image>(selectSql);
+            return View(listImage);
+        }
         #endregion
+
+        #region Create()
         public IActionResult Create()
         {
+            var versions = DBUtl.GetList("SELECT version_id, name FROM version ORDER BY name");
+            ViewData["versions"] = new SelectList(versions, "version_id", "name");
             return View();
         }
-        #region Details(int id)
-        [AllowAnonymous]
-        public IActionResult Details(int id)
-        {
-            string sql =
-               @"SELECT h.*, 
-                     u.FullName AS SubmittedBy
-                FROM TravelHighlight h, TravelUser u
-               WHERE h.UserId = u.UserId
-                 AND Id={0}";
 
-            List<Trip> lstTrip = DBUtl.GetList<Trip>(sql, id);
-            if (lstTrip.Count == 1)
+        [HttpPost]
+        public IActionResult Create(Wound wound, Image image)
+        {
+            if (!ModelState.IsValid)
             {
-                Trip trip = lstTrip[0];
-                return View("Details", trip);
+                ViewData["Msg"] = "Invalid Input";
+                ViewData["MsgType"] = "warning";
+                return View("Create");
             }
             else
             {
-                TempData["Message"] = "Trip Record does not exist";
-                TempData["MsgType"] = "warning";
-                return RedirectToAction("Index");
+
+                string picfilename = DoPhotoUpload(image.Photo);
+
+                string woundSql = @"INSERT INTO wound(name, wound_stage, remarks,
+                                    wound_category_id, wound_location_id, 
+                                    tissue_id, version_id, image_id, user_id)
+                                    VALUES('{0}','{1}','{2}',{3},{4},{5},{6},{7},{8})";
+                int woundRowsAffected = DBUtl.ExecSQL(woundSql, wound.name, wound.wound_stage, wound.remarks,
+                    wound.wound_category_id, wound.wound_location_id, wound.tissue_id,
+                    wound.version_id, wound.image_id, wound.user_id);
+
+                string imageSql = @"INSERT INTO image(name, type, img_file)
+                                    VALUES('{0}','{1}','{2}')";
+                int imageRowsAffected = DBUtl.ExecSQL(imageSql, image.name, image.type, picfilename);
+
+                if (woundRowsAffected == 1 && imageRowsAffected == 1)
+                {
+                    TempData["Msg"] = "New wound created";
+                    TempData["MsgType"] = "success";
+                    return RedirectToAction("Details");
+                }
+                else
+                {
+                    TempData["Msg"] = DBUtl.DB_Message;
+                    TempData["MsgType"] = "danger";
+                    return View("Create");
+                }
             }
+        }
+        #endregion
+
+        #region DoPhotoUpload()
+        private string DoPhotoUpload(IFormFile photo)
+        {
+            string fext = Path.GetExtension(photo.FileName);
+            string uname = Guid.NewGuid().ToString();
+            string fname = uname + fext;
+            string fullpath = Path.Combine(_env.WebRootPath, "photos/" + fname);
+            using (FileStream fs = new FileStream(fullpath, FileMode.Create))
+            {
+                photo.CopyTo(fs);
+            }
+            return fname;
+        }
+
+        private IWebHostEnvironment _env;
+        public WoundController(IWebHostEnvironment environment)
+        {
+            _env = environment;
         }
         #endregion
     }
---
+++ ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Models/Wound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WoundImgRepo.Models
+{
+    public class Wound
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string wound_stage { get; set; }
+        public string remarks { get; set; }
+        public int wound_category_id { get; set; }
+        public int wound_location_id { get; set; }
+        public int tissue_id { get; set; }
+        public int version_id { get; set; }
+        public int image_id { get; set; }
+        public int user_id { get; set; }
+    }
+}
