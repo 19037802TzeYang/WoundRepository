1f5fa160c18ff4dae2d2d4f9ef802877465077a6
GABRIEL PETER LIANGCO BANAL RP <19044593@myrp.edu.sg>
2021-06-21 22:54:44 +0800

    Changes made on AccountController. Trying to do user Logoff..

--- ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/AccountController.cs
+++ ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/AccountController.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.Security.Claims;
+using WoundImgRepo.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WoundImgRepo.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        [Authorize]
+        public IActionResult Logoff(string returnUrl = null)
         {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            return RedirectToAction("LoginPage", "Login");
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            TempData["ReturnUrl"] = returnUrl;
             return View();
         }
     }
