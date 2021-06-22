e375c588df9cc00590b7cad7fb7e6202d4382a22
GABRIEL PETER LIANGCO BANAL RP <19044593@myrp.edu.sg>
2021-06-22 23:55:06 +0800

    Testing the AccountController with reference to C236 L09

--- ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/AccountController.cs
+++ ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/AccountController.cs
@@ -26,5 +26,71 @@ namespace WoundImgRepo.Controllers
             TempData["ReturnUrl"] = returnUrl;
             return View();
         }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(LogInUser user)
+        {
+            if (!AuthenticateUser(user.Username, user.Password,
+                                  out ClaimsPrincipal principal))
+            {
+                ViewData["Message"] = "Incorrect User ID or Password";
+                return View();
+            }
+            else
+            {
+                HttpContext.SignInAsync(
+                   CookieAuthenticationDefaults.AuthenticationScheme,
+                   principal);
+
+                // Update the Last Login Timestamp of the User
+                string update = "UPDATE useracc SET last_login=GETDATE() WHERE username='{0}'";
+                DBUtl.ExecSQL(update, user.Username);
+
+                if (TempData["returnUrl"] != null)
+                {
+                    string returnUrl = TempData["returnUrl"].ToString();
+                    if (Url.IsLocalUrl(returnUrl))
+                        return Redirect(returnUrl);
+                }
+
+                return RedirectToAction("LoginPage", "Login");
+            }
+        }
+
+        [AllowAnonymous]
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+
+        private bool AuthenticateUser(string uid, string pw,
+                                         out ClaimsPrincipal principal)
+        {
+            principal = null;
+
+
+            string sql = @"SELECT * FROM useracc 
+                         WHERE username = '{0}' AND password = HASHBYTES('SHA1', '{1}')";
+
+            // TODO: L09 Task 1 - Make Login Secure, use the new way of calling DBUtl
+            //string select = String.Format(sql, uid, pw);
+            DataTable ds = DBUtl.GetTable(sql, uid, pw);
+            //DataTable ds = DBUtl.GetTable(select);
+            if (ds.Rows.Count == 1)
+            {
+                principal =
+                   new ClaimsPrincipal(
+                      new ClaimsIdentity(
+                         new Claim[] {
+                        new Claim(ClaimTypes.NameIdentifier, uid),
+                        new Claim(ClaimTypes.Name, ds.Rows[0]["username"].ToString()),
+                        new Claim(ClaimTypes.Role, ds.Rows[0]["user_role"].ToString())
+                         },
+                         CookieAuthenticationDefaults.AuthenticationScheme));
+                return true;
+            }
+            return false;
+        }
+
     }
 }
