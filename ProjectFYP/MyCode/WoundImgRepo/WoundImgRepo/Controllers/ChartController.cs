8a54381a27bc19307c89702cecfa8ea13c38d8cf
JUSTIN OON JING YANG RP <19041265@myrp.edu.sg>
2021-07-11 17:45:33 +0800

    Made a second table to dashboard to display by category

--- ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/ChartController.cs
+++ ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/ChartController.cs
@@ -24,12 +24,19 @@ namespace WoundImgRepo.Controllers
         public IActionResult WoundsLocationRecords()
         {
             PrepareWoundsData(1);
-            ViewData["Chart"] = "pie";
-            ViewData["Title"] = "Version";
-            ViewData["ShowLegend"] = "true";
+            ViewData["Chart"] = "bar";
+            ViewData["Title"] = "Location";
+            ViewData["ShowLegend"] = "false";
             return View("WoundsLocationRecords");
         }
-
+        public IActionResult DisplayCategories()
+        {
+            WoundCategoryRecords(1);
+            ViewData["Chart"] = "pie";
+            ViewData["Title"]="Category";
+            ViewData["ShowLegend"] = "true";
+            return View("DisplayCategories");
+        }
         private void PrepareData(int x)
         {
             int[] version = new int[2] { 0, 0 }; 
@@ -61,7 +68,7 @@ namespace WoundImgRepo.Controllers
         private void PrepareWoundsData(int x)
         {
             int[] wounds = new int[] { 0, 0, 0 };
-            int length = wounds.Length;
+            
             List<Wound> list = DBUtl.GetList<Wound>("SELECT * FROM wound");
             List<WoundLocation> wLocation = DBUtl.GetList<WoundLocation>("SELECT * FROM wound_location");
             
@@ -98,6 +105,40 @@ namespace WoundImgRepo.Controllers
                 ViewData["Colors"] = new[] { "gray", "darkgray", "black" };
                 ViewData["Labels"] = new[] { "X", "Y", "Z" };
                 ViewData["Data"] = new int[] { 1, 2, 3 };
+            }
+        }
+
+        private void WoundCategoryRecords(int x)
+        {
+            int[] categories = new int[] { 0, 0, 0 };
+            List<Wound> list = DBUtl.GetList<Wound>("SELECT * FROM wound");
+            List<WoundCategory> categoryList = DBUtl.GetList<WoundCategory>("SELECT * FROM wound_category");
+
+            foreach (Wound w in list)
+            {
+                categories[calculatePosition(w.wound_category_id)]++;
+
+            }
+            if (x == 1)
+            {
+                ViewData["Legend"] = "Wounds";
+                string[] knownColors = new string[3] { "grey", "brown", "black" };
+                string[] colors = new string[categories.Length];
+                string[] labels = new string[categories.Length];
+                for (int i = 0; i < colors.Length; i++)
+                {
+
+                    colors[i] = knownColors[i];
+
+                }
+                for (int i = 0; i < categoryList.Count; i++)
+                {
+
+                    labels[i] = categoryList[i].name;
+                }
+                ViewData["Colors"] = colors;
+                ViewData["Labels"] = labels;
+                ViewData["Data"] = categories;
             }
         }
         private int calculatePosition(int x)
