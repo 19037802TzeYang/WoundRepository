9d177bcbabb9d7e54a043a5772696d2a3b52b3bb
19024626-Stephen Ng <82921843+19024626@users.noreply.github.com>
2021-07-21 17:49:42 +0800

    fixed remember.Me

--- ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/AccountController.cs
+++ ProjectFYP/MyCode/WoundImgRepo/WoundImgRepo/Controllers/AccountController.cs
@@ -30,7 +30,7 @@ namespace WoundImgRepo.Controllers
         [AllowAnonymous]
         public IActionResult LoginPage(string returnUrl = null)
         {
-            #region checkuserrole()
+            #region RememberMe()
             int checktheuserrole = 0;
             if (User.IsInRole("Admin"))
             {
@@ -53,7 +53,7 @@ namespace WoundImgRepo.Controllers
 
             else
             {
-                return RedirectToAction("Index", "Wound");
+                return RedirectToAction("TheWounds", "Wound");
             }
             
         }
@@ -127,7 +127,7 @@ namespace WoundImgRepo.Controllers
                 
                 System.Diagnostics.Debug.WriteLine("login success!");
                 current_user = user.Username;
-                return RedirectToAction("Index", "Wound");
+                return RedirectToAction("TheWounds", "Wound");
             }
         }
 
